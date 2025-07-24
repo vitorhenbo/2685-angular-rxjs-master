9fb841ca968bd528645041b915d4ae2f0e28a1ae
Vitor <vitorhenriquebo@yahoo.com.br>
2025-07-24 20:34:25 -0300

    adiciona funcionalidade de busca de livros com integração ao serviço de livros

---
+++ src/app/service/livro.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LivroService {
+  private readonly API = 'https://www.googleapis.com/books/v1/volumes';
+
+  constructor(private httpClient: HttpClient) {}
+
+  public buscar(valorDigitado: string) {
+    const params = new HttpParams().append('q', valorDigitado);
+    return this.httpClient.get<any>(this.API, { params });
+  }
+}
--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,17 +1,19 @@
 import { Component } from '@angular/core';
+import { LivroService } from 'src/app/service/livro.service';
 
 @Component({
   selector: 'app-lista-livros',
   templateUrl: './lista-livros.component.html',
-  styleUrls: ['./lista-livros.component.css']
+  styleUrls: ['./lista-livros.component.css'],
 })
 export class ListaLivrosComponent {
+  public campoBusca = '';
 
   listaLivros: [];
 
-  constructor() { }
+  constructor(private readonly livroService: LivroService) {}
 
+  public buscarLivros() {
+    this.livroService.buscar(this.campoBusca).subscribe({});
+  }
 }
-
-
-
