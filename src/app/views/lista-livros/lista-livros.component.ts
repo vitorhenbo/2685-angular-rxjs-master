19e06e1da14dff69b6ddf1b6da984971f836155a
Vitor <vitorhenriquebo@yahoo.com.br>
2025-07-31 19:35:52 -0300

    adiciona gerenciamento de assinatura no componente de lista de livros

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { LivroService } from 'src/app/service/livro.service';
 
 @Component({
@@ -6,18 +7,22 @@ import { LivroService } from 'src/app/service/livro.service';
   templateUrl: './lista-livros.component.html',
   styleUrls: ['./lista-livros.component.css'],
 })
-export class ListaLivrosComponent {
+export class ListaLivrosComponent implements OnDestroy {
   public campoBusca = '';
-
   listaLivros: [];
+  private subscription: Subscription;
 
   constructor(private readonly livroService: LivroService) {}
 
   public buscarLivros() {
-    this.livroService.buscar(this.campoBusca).subscribe({
+    this.subscription = this.livroService.buscar(this.campoBusca).subscribe({
       next: (retornoAPI) => console.log(retornoAPI),
       error: (erro) => console.error(erro),
       complete: () => console.log('Observable completado'),
     });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
